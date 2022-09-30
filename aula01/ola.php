b402e9cfe8401dd5adfc5f5962cc1a5631b429ec
Simon Gama <simongama@hotmail.com>
2022-09-30 18:36:27 -0300

    aula01
    Atualização

--- aula01/ola.php
+++ aula01/ola.php
@@ -8,10 +8,28 @@
 </head>
 <body>
     <?php
-    echo "Olá, meu primeiro programa em PHP.";
-?>
+    $linguagem = "HTML, CSS e PHP";
+    echo "<h1>Olá, meu primeiro programa em " . $linguagem . "</h1>";
 
+    $linguagem = "HTML, CSS e PHP";
+    echo "Olá, meu primeiro programa em . $linguagem";
 
+    printf ("<br>Olá, meu primeiro programa em . $linguagem");
+
+    ?>
+
+    <?php
+    //Declaração de variável//
+    $num1;
+    //Atribuição de valor a variável//
+    $num=10;
+    //Declarando a variável e atribuindo valor.
+    $num2 ="5"; //Varável do tipo string
+    //Soma realizada com sucesso, mesmo sendo uma das variáveis string.
+    $total= ($num1 + $num2);
+    echo " A soma de ".$num1." + ".$num2." = ". ($num1 + $num2);
+ 
+    ?>
 
     
 </body>
