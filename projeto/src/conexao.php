27fcba816ff43cfaedd6f18c5aa1646c7356d9f5
Simon Gama <simongama@hotmail.com>
2022-11-25 21:15:19 -0300

    aula

--- projeto/src/conexao.php
+++ projeto/src/conexao.php
@@ -1,11 +1,14 @@
 <?php
 
 $hostname = "localhost";
-//$banco_de_dados = "projeto";
-$banco_de_dados = "id19904110_projetobd"
-//$usuario = "root";
-$usuario = "id19904110_desenvolvedor";
-$senha = "B47UN3#{DJ+v_X[V";
+$banco_de_dados = "projeto";
+$usuario = "root";
+$senha = "";
+
+//$hostname = "localhost";
+//$banco_de_dados = "id19904110_projetobd";
+//$usuario = "id19904110_desenvolvedor";
+//$senha = "B47UN3#{DJ+v_X[V";
 
 $conexao = new mysqli($hostname, $usuario, $senha, $banco_de_dados);
 if($conexao->connect_errno){ // error + number = errno
@@ -13,3 +16,5 @@ if($conexao->connect_errno){ // error + number = errno
 } else {
     // echo "Conectado ao Banco.<br>";
 }
+
+
